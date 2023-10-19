3735f73077ef8a3eb205af0a0e3bc70f70f9796d
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-10-19 14:59:04 -0700

    Update and rename 22 Getting Distinct Values.py to 22 Getting Distinct Values.sql

--- 22 Getting Distinct Values.sql
+++ 22 Getting Distinct Values.sql
@@ -1,12 +1,12 @@
-# Create query for unique combinations of borough and complaint_type
+-- Create query for unique combinations of borough and complaint_type
 query = """
 SELECT DISTINCT borough, 
        complaint_type
   FROM hpd311calls;
 """
 
-# Load results of query to a dataframe
+-- Load results of query to a dataframe
 issues_and_boros = pd.read_sql(query, engine)
 
-# Check assumption about issues and boroughs
+-- Check assumption about issues and boroughs
 print(issues_and_boros)
